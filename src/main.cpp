d688d9658704543ec5914c99cc4267803c5803d6
Hatuanvuong <tuanquach17022001@gmail.com>
2023-11-11 16:31:20 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// put function declarations here:
+//int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  //int result = myFunction(2, 3);
+  pinMode(18, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(18, HIGH);
+  delay(1000);
+  digitalWrite(18, LOW);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+/*int myFunction(int x, int y) {
+  return x + y;
+}*/
